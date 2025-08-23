bcd837ff731559fa666837b8c3d1479456c04654
Elkin <ed.penar1@uniandes.edu.co>
2025-08-23 10:46:30 -0500

    Se actualiza models

--- app/models.py
+++ app/models.py
@@ -21,5 +21,27 @@ class RegistroDestajo(db.Model):
     fecha_registro = db.Column(db.DateTime, default=datetime.utcnow, nullable=False)
     usuario_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     actualizado_en = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
+    sincronizado = db.Column(db.Boolean, default=False)
+    last_sync = db.Column(db.DateTime)
 
     usuario = db.relationship('User', backref='destajos')
+
+class GHDestajo(db.Model):
+    __tablename__ = "GH_Destajos"
+    Id = db.Column(db.Integer, primary_key=True)
+    Planta = db.Column(db.String(200))
+    Concepto = db.Column(db.String(200))
+    Valor = db.Column(db.Float)
+
+class GHEmpleado(db.Model):
+    __tablename__ = "GH_Empleados"
+    numeroDocumento = db.Column(db.String(50), primary_key=True)
+    tipoIdentificacion = db.Column(db.String(50))
+    nombreCompleto = db.Column(db.String(200))
+    apellidoCompleto = db.Column(db.String(200))
+    cargo = db.Column(db.String(200))
+    centroCosto = db.Column(db.String(200))
+    estado = db.Column(db.String(50))
+    nombreNomina = db.Column(db.String(200))
+    compania = db.Column(db.String(200))
+    agrupador4 = db.Column(db.String(200))
