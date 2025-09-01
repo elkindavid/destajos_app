8da2f46276f33382dab780ffc3f6ecd8d04d2d76
Elkin <ed.penar1@uniandes.edu.co>
2025-09-01 01:13:24 -0500

    sw test

--- app/pwa.py
+++ app/pwa.py
@@ -1,13 +1,13 @@
 from flask import Blueprint, send_from_directory, make_response
 
-pwa_bp = Blueprint('pwa', __name__, static_folder='static')
+pwa_bp = Blueprint('pwa', __name__)
 
 @pwa_bp.route('/manifest.json')
 def manifest():
-    return send_from_directory('static', 'manifest.json')
+    return send_from_directory('.', 'manifest.json')
 
-@pwa_bp.route('/service-worker.js')
+@pwa_bp.route('/sw.js')
 def service_worker():
-    response = make_response(send_from_directory('static', 'service-worker.js'))
+    response = make_response(send_from_directory('.', 'sw.js'))
     response.headers['Content-Type'] = 'application/javascript'
     return response
